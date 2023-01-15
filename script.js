58d703ff9bc3af36458a9cc5975a5115793ba3c1
simonisdev <sagredo83@gmail.com>
2023-01-15 02:21:41 -0300

    botón scroll back-to-top

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// import smoothscroll from 'smoothscroll-polyfill';
+
 const form = document.getElementById("contact-form");
 
 form.addEventListener("submit", event => {
@@ -8,6 +10,7 @@ form.addEventListener("submit", event => {
   // aquí puedes enviar los datos del formulario a un servidor mediante una petición AJAX
   console.log(name, email, message);
 });
+
 
 const backToTopButton = document.getElementById("back-to-top");
 
@@ -21,6 +24,28 @@ window.onscroll = function() {
 };
 
 // regresar al inicio de la pagina cuando se haga click en el boton
+// backToTopButton.onclick = function() {
+//   window.scrollTo(0, 0);  
+// };
+
+
+// smoothscroll.polyfill();
+
+// backToTopButton.onclick = function () {
+//   window.scroll({
+//     top: 0,
+//     left: 0,
+//     behavior: 'smooth'
+//   });
+// };
+
+
 backToTopButton.onclick = function() {
-  window.scrollTo(0, 0);
+  const scrollStep = -window.scrollY / (500 / 15),
+        scrollInterval = setInterval(function(){
+        if ( window.scrollY !== 0 ) {
+            window.scrollBy( 0, scrollStep );
+        }
+        else clearInterval(scrollInterval); 
+    },15);
 };
